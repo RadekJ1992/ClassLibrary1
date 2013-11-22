d366575ad3c8609c6562f8545c6537c0450065b6
Radosław Jarzynka <radoslaw.jarzynka@gmail.com>
2013-11-23 00:12:44 +0100

    track portvpivci on!

---
+++ ClassLibrary1/PortVPIVCI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packet {
+    public class PortVPIVCI {
+        public int port;
+        public int VPI;
+        public int VCI;
+
+        public PortVPIVCI(int port,int VPI, int VCI) {
+            this.port = port;
+            this.VPI = VPI;
+            this.VCI = VCI;
+        }
+        public PortVPIVCI() {}
+    }
+
+    public class PortVPIVCIComparer : IEqualityComparer<PortVPIVCI> {
+
+        public bool Equals(PortVPIVCI portvpivci1, PortVPIVCI portvpivci2) {
+            bool temp = false;
+            if (portvpivci1.port == portvpivci2.port && portvpivci1.VCI == portvpivci2.VCI && portvpivci1.VPI == portvpivci2.VPI) temp = true;
+            return temp;
+        }
+
+        public int GetHashCode(PortVPIVCI portvpivci) {
+            return portvpivci.port.GetHashCode() + portvpivci.VPI.GetHashCode() + portvpivci.VCI.GetHashCode();
+        }
+
+    }
+}
