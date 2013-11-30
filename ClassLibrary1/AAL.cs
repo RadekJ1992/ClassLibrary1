c53d879dbd871058df25069647d72a4ecc5da657
Radosław Jarzynka <radoslaw.jarzynka@gmail.com>
2013-11-30 17:18:39 +0100

    teraz już normalnie pobiera bity ze stringu

--- ClassLibrary1/AAL.cs
+++ ClassLibrary1/AAL.cs
@@ -18,8 +18,9 @@ namespace Packet {
     public class AAL {
 
         public static byte[] GetBytesFromString(string str) {
-            byte[] bytes = new byte[str.Length * sizeof(char)];
-            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+           // byte[] bytes = new byte[str.Length * sizeof(char)];
+           // System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            Byte[] bytes = System.Text.Encoding.ASCII.GetBytes(str);
             return bytes;
         }
 
