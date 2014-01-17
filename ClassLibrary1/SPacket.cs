61f6b6112fdd602059a59190ec3afdd550b3db14
Radosław Jarzynka <radoslaw.jarzynka@gmail.com>
2014-01-17 18:33:38 +0100

    zmiana namespace by pasował do reszty

--- ClassLibrary1/SPacket.cs
+++ ClassLibrary1/SPacket.cs
@@ -6,7 +6,7 @@ using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SignalPacket 
+namespace Packet 
 {   
     [Serializable]
     class SPacket : ISerializable
